56870e7ad266116003d0b96c63b17c06dc54d77e
Mateus Felipe <brasil.mateus748@gmail.com>
2024-09-20 02:21:59 -0300

    Add imports and a div

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar'
+import Home from './components/Home'
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <div className="content">
+        <h1>App Component</h1>
+      </div>
     </div>
   );
 }
