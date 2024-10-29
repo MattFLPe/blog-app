4aaf1bca4e4e019c33e5a6d6e8851e38458f5ca1
Mateus Felipe <brasil.mateus748@gmail.com>
2024-10-28 23:57:03 -0300

    Added three mock blog posts Added prop

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,15 @@
 import { useState } from "react";
+import BlogList from "../BlogList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
-        { title: 'My first blog', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Productivity tips', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+        { title: "My first blog", body: "lorem ipsum...", author: "Joe", id: 1 },
+        { title: "Writing tips", body: "lorem ipsum...", author: "Matt", id: 2 },
+        { title: "Show, don't tell", body: "lorem ipsum...", author: "Carl", id: 3 }
     ])
-    
     return ( 
         <div className="home">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}> 
-                <h2>{blog.title}</h2>
-                <p>Written by {blog.author}</p>
-                </div>
-            ))}
+            <BlogList blogs={blogs} title='All Blogs!'/>
         </div>
      );
 }
