00cd36b140d7f018ebca8cd6e6d4f4748b787bce
Mateus Felipe <brasil.mateus748@gmail.com>
2024-10-31 23:57:30 -0300

    Destructured prop in the BlogList component

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,11 +1,11 @@
 import { useState } from "react";
 
-const BlogList = (props) => {
-   const blogs = props.blogs;
-   const title = props.title;
+const BlogList = ({blogs, title}) => {
+
    
     return ( 
         <div className="blog-list">
+        <h2>{title}</h2>
         {blogs.map((blog) => (
             <div className="blog-preview" key={blog.id}> 
             <h2>{blog.title}</h2>
