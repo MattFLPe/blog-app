e8d2b6be63e6e7058a26efcf9cfbabe3d640a9db
Mateus Felipe <brasil.mateus748@gmail.com>
2024-11-23 23:46:54 -0300

    Added method that filters blogs by category

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,12 +4,14 @@ import BlogList from "../BlogList";
 const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: "My first blog", body: "lorem ipsum...", author: "Joe", id: 1 },
-        { title: "Writing tips", body: "lorem ipsum...", author: "Matt", id: 2 },
-        { title: "Show, don't tell", body: "lorem ipsum...", author: "Carl", id: 3 }
+        { title: "Promises - Asynchronous JS", body: "lorem ipsum...", author: "Matt", category: "JavaScript", id: 2 },
+        { title: "Java", body: "lorem ipsum...", author: "Carl", category: "literature", id: 3 },
+        { title: "Props - ReactJS", body: "lorem ipsum...", author: "Matt", category: "ReactJS", id: 4 },
     ])
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title='All Blogs!'/>
+            <BlogList blogs={blogs} title='All Blogs'/>
+            <BlogList blogs={blogs.filter((blog) => blog.category === 'JavaScript')} title='JavaScript Blogs'/>
         </div>
      );
 }
